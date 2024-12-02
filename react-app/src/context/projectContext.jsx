d3158b5ed2db5e7ae0adee421059b99cb0a91a0c
Maya Eid <mayabaseleid@gmail.com>
2024-12-02 12:16:57 +0200

    frontend: fetch all projects and set in state

---
+++ react-app/src/context/projectContext.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const ProjectsContext = createContext();
+
+const ProjectsProvider = ({ children }) => {
+    const [projects, setProjects] = useState([]);
+  
+    useEffect(() => {
+      const fetchProjects = async () => {
+        try {
+          const response = await axios.get("http://127.0.0.1:8000/api/projects/get_projects");
+          setProjects(response.data.projects);
+        } catch (error) {
+          console.error("Failed to fetch projects:", error);
+        }
+      };
+  
+      fetchProjects();
+    }, []);
+  
+    return (
+    );
+  };
+  
+  export default ProjectsProvider;
