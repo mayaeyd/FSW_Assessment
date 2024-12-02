d821372e51dea989d243902d56938590749e427d
Maya Eid <mayabaseleid@gmail.com>
2024-12-02 11:27:46 +0200

    backend: create get projects api

--- laravel/app/Http/Controllers/ProjectsController.php
+++ laravel/app/Http/Controllers/ProjectsController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
-    //
+    public function get_projects(Request $request){
+        $projects = Project::all();
+        return response()->json([
+            "projects"=>$projects,
+        ]);
+    }
 }
