45b851ae95dab81a243ccb1df7d53a24a4ba4777
Maya Eid <mayabaseleid@gmail.com>
2024-12-02 12:18:53 +0200

    frontend: wrap app with projects provider

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Projects from "./pages/Projects";
+import ProjectsProvider from "./context/projectContext";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </BrowserRouter>
+      <ProjectsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </BrowserRouter>
+      </ProjectsProvider>
     </div>
   );
 }
