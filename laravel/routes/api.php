6dc0775bd79d1e44c0c06aa1e8a52e4c2748b281
Maya Eid <mayabaseleid@gmail.com>
2024-12-02 11:25:59 +0200

    backend: create routes for projects CRUD

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\ProjectsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix("/projects")->group(function(){
+    Route::get("/",[ProjectsController::class,"get_projects"]);
+    Route::post("/",[ProjectsController::class,"create_project"]);
+    Route::get("/{id}",[ProjectsController::class,"get_project"]);
+    Route::put("/{id}",[ProjectsController::class,"update_project"]);
+    Route::delete("/{id}",[ProjectsController::class,"delete_project"]);
+});
