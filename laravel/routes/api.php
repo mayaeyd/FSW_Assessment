8e57a3aa0ad96f690709fd7ac5344c44b196c216
Maya Eid <mayabaseleid@gmail.com>
2024-12-02 11:37:48 +0200

    backend: add routes for users apis

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -11,3 +11,11 @@ Route::prefix("/projects")->group(function(){
     Route::put("/{id}",[ProjectsController::class,"update_project"]);
     Route::delete("/{id}",[ProjectsController::class,"delete_project"]);
 });
+
+Route::prefix("/users")->group(function(){
+    Route::get("/",[ProjectsController::class,"get_users"]);
+    Route::post("/",[ProjectsController::class,"create_user"]);
+    Route::get("/{id}",[ProjectsController::class,"get_user"]);
+    Route::put("/{id}",[ProjectsController::class,"update_user"]);
+    Route::delete("/{id}",[ProjectsController::class,"delete_user"]);
+});
