a3b0b36430cb4d1aa0fb16545929e269e866d5a6
Maya Eid <mayabaseleid@gmail.com>
2024-12-02 11:36:21 +0200

    backend: create CRUD apis for users

--- laravel/app/Http/Controllers/UsersController.php
+++ laravel/app/Http/Controllers/UsersController.php
@@ -2,9 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    public function get_users(Request $request)
+    {
+        $users = User::all();
+        return response()->json([
+            "users" => $users,
+        ]);
+    }
+
+    public function create_user(Request $request)
+    {
+        $user = User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => bcrypt($request->password),
+        ]);
+
+        return response()->json([
+            "created_user" => $user,
+        ]);
+    }
+
+    public function update_user(Request $request)
+    {
+        $user = User::find($request->id);
+        
+        if (!$user) {
+            return response()->json([
+                "error" => "User not found",
+            ], 404);
+        }
+
+        $user->update([
+            "name" => $request->name,
+            "email" => $request->email,
+        ]);
+
+        return response()->json([
+            "updated_user" => $user,
+        ]);
+    }
+
+    public function delete_user(Request $request)
+    {
+        $user = User::find($request->id);
+
+        if (!$user) {
+            return response()->json([
+                "error" => "User not found",
+            ], 404);
+        }
+
+        $user->delete();
+
+        return response()->json([
+            "deleted_user" => $user,
+        ]);
+    }
+
+    public function get_user(Request $request)
+    {
+        $user = User::find($request->id);
+
+        if (!$user) {
+            return response()->json([
+                "error" => "User not found",
+            ], 404);
+        }
+
+        return response()->json([
+            "user" => $user,
+        ]);
+    }
 }
